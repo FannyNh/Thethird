610e25f25c0f3c5b360afaaa79b9207ca19286a5
fnhouyvanisvong <=>
2022-07-25 11:32:04 +0200

    init portfolio

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Main />
+  </div>
+</template>
+
+<script>
+import "@/assets/styles/reset.css";
+import "@/assets/styles/global.css";
+
+import Main from "./components/Main.vue";
+
+export default {
+  name: "App",
+  components: {
+    Main,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&display=swap");
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,193 @@
+<template>
+  <div class="hero">
+    <!-- <img class="background" src="@/assets/img/background.svg" />  Shadows version -->
+    <div>
+      <img class="background" src="@/assets/img/background.svg" />
+    </div>
+    <div class="hero__container">
+      <div class="hero__main">
+        <h5 class="hero__title color--dk-purple">Fanny Nhouyvanisvong</h5>
+        <h3 class="hero__sub-title color--md-orange">Creative and Front-End Developer</h3>
+        <a
+          class="hero__btn"
+          href="mailto:nhouy.fanny@gmail.com"
+          subject="subject text"
+          target="__blank"
+          title="my e-mail"
+          >Message Me</a
+        >
+      </div>
+      <div class="hero__footer">
+        <li class="hero__links color--dk-purple">
+          <ul>
+            <a
+              class="hero__link"
+              href="https://drive.google.com/file/d/1NB9uMEngF4lBcWSPgODz0ykahVCGYaHQ/view?usp=sharing"
+              target="__blank"
+              title="my Resume"
+              >Resume</a
+            >
+          </ul>
+          <ul>
+            <a
+              class="hero__link"
+              href="https://github.com/FannyNh"
+              target="__blank"
+              title="my GitHub"
+              >Github</a
+            >
+          </ul>
+          <ul>
+            <a
+              class="hero__link"
+              href="https://www.linkedin.com/in/tomasz-swierad/"
+              target="__blank"
+              title="Tomasz Świerad LinkedIn"
+              >LinkedIn</a
+            >
+          </ul>
+          <ul>
+            <a
+              class="hero__link"
+              href="https://www.instagram.com/the_third_moira/"
+              target="__blank"
+              title="my instagram"
+              >Instagram</a
+            >
+          </ul>
+        </li>
+        <div class="hero__copyright color--dk-purple">© 2022 the third</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  components: {},
+};
+</script>
+
+<style scoped>
+.background {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.hero__container {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  position: relative;
+  max-width: 1480px;
+  height: 100vh;
+  margin: 0px auto;
+  padding: 0px 20px 0px 20px;
+}
+.hero__main {
+  max-width: 678px;
+  width: 100%;
+  margin: 0 auto;
+  padding-top: 8%;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+}
+.hero__title {
+  font-size: 72px;
+  font-weight: 700;
+  line-height: 91px;
+}
+.hero__sub-title {
+  font-size: 36px;
+  font-weight: 400;
+  line-height: 45px;
+  padding-top: 10px;
+}
+.hero__btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 120px;
+  height: 40px;
+  background-color: #ff8e00;
+  border-radius: 8px;
+  font-size: 14px;
+  margin: 48px auto 0px;
+}
+.hero__btn:hover {
+  opacity: 0.8;
+  transition: 0.5s;
+}
+.hero__footer {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-bottom: 30px;
+  max-height: 200px;
+  height: 100%;
+}
+.hero__links {
+  display: flex;
+  margin: auto;
+  max-width: 536px;
+  width: 100%;
+  justify-content: space-between;
+  font-size: 14px;
+  line-height: 18px;
+  font-weight: 400;
+}
+.hero__link:hover {
+  transition: 0s;
+  font-weight: 700;
+}
+.hero__copyright {
+  font-size: 10px;
+  margin: auto;
+  margin: 0 auto;
+}
+@media (max-width: 707px) {
+  .hero__main {
+    padding-top: 50px;
+  }
+}
+@media (max-width: 555px) {
+  .hero__main {
+    padding-top: 40px;
+  }
+  .hero__title {
+    line-height: 70px;
+  }
+  .hero__title {
+    font-size: 65px;
+    line-height: 60px;
+  }
+  .hero__sub-title {
+    font-size: 26px;
+  }
+  .hero__btn {
+    margin: 40px auto 70px;
+  }
+}
+@media (max-width: 480px) {
+  .hero__title {
+    font-size: 35px;
+    line-height: 30px;
+  }
+  .hero__btn {
+    margin-bottom: 40px;
+  }
+  .hero__links {
+    flex-direction: column;
+    align-items: center;
+    gap: 15px;
+  }
+  .hero__copyright {
+    font-size: 8px;
+    margin-top: 25px;
+  }
+}
+</style>
