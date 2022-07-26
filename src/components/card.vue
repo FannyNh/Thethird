93e5ae6a07928ce3e0758a828f2b5d1283e1d85d
fnhouyvanisvong <=>
2022-07-26 19:49:16 +0200

    add card component

---
+++ src/components/card.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="card">
+<div class="card__image">
+  <img alt="image carte" src="" />
+</div>
+  <div class="card__body card-body">
+    <h5 class="card-body__title"></h5>
+    <h6 class="card-body__sub-title"></h6>
+    <p class="card-body__body"></p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "card"
+}
+</script>
+
+<style scoped>
+
+</style>
